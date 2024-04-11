3ecda7de925f3425b53ef755d0e9c05558da714a
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2024-04-11 18:44:27 -0300

    more level design

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x,0,-8);
+    }
+}
