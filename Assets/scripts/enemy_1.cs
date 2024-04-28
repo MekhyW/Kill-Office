58e829b22b9a9582d90da741ebe173488aeeae4f
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-04-28 11:38:43 -0300

    Inimigo patrulha e atira quando vê o player

--- Assets/scripts/enemy_1.cs
+++ Assets/scripts/enemy_1.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class enemy_1 : MonoBehaviour
 {
 
     private Animator animator;
+    private bool IsMovingRight = true;
+    private bool IsMovingLeft = false;
+    private bool IsShooting = false;
+    private bool ShootIsReady = true;
+
+    private bool IsDead = false;
     
     
     public GameObject bullet,my_bullet;
@@ -17,26 +24,118 @@ public class enemy_1 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        if (IsDead){
+            return;
+        }
+        Walk();
+        EnemySight();
     }
 
     public void shoot(){
-        my_bullet = Instantiate(bullet, transform.position - new Vector3(0.5f,-0.7f,0f), Quaternion.identity);
+        my_bullet = Instantiate(bullet, transform.position - new Vector3(0.5f,-0.7f,0f), Quaternion.identity); 
         my_bullet.gameObject.GetComponent<bullet>().setSpeed(-2f * transform.localScale.x);
         animator.SetBool("shoot",false);
-        StartCoroutine(delayShoot());
+        IsShooting = false;
+        StartCoroutine(delayShoot()); 
     }
 
     public IEnumerator delayShoot(){
 
-        yield return new WaitForSeconds(2f);
-        animator.SetBool("shoot",true);
+        yield return new WaitForSeconds(1f);
+        ShootIsReady = true;
+    }
+
+    private void EnemySight(){
+        Vector2 size = new Vector2(0.5f,0.5f);
+        Vector2 direction = Vector2.right * transform.localScale.x * size.x;
+        Vector2 offset = new Vector2(0f,0.5f);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + offset, direction, 10f);
+        RaycastHit2D hit2 = Physics2D.Raycast((Vector2)transform.position + offset, Vector2.up, 10f);
+        if (hit.collider != null){
+            print(hit.collider.gameObject.tag);
+        }
+        // draw a line to see the raycast
+        Debug.DrawRay((Vector2)transform.position + offset, direction, Color.green);
+        Debug.DrawRay((Vector2)transform.position + offset, Vector2.up, Color.red);
+        if (hit.collider != null && hit.collider.gameObject.tag == "Player")
+        {
+            if (ShootIsReady){
+                ShootIsReady = false;
+                animator.SetBool("shoot",true);
+                IsShooting = true;
+            }
+            else{
+                print("not ready to shoot");
+            }
+        }
+        if(hit2.collider != null && hit2.collider.gameObject.tag == "Player"){
+                if (IsMovingLeft){
+                transform.localScale = new Vector3(1f,1f,1f); // flip the sprite
+                IsMovingRight = true;
+                IsMovingLeft = false;
+            }
+            else if(IsMovingRight){
+                transform.localScale = new Vector3(-1f,1f,1f); // flip the sprite
+                IsMovingRight = false;
+                IsMovingLeft = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag == "EnemyWaypointA"){
+            //print("hit waypoint A");
+            transform.localScale = new Vector3(1f,1f,1f); // flip the sprite
+            IsMovingRight = true;
+            IsMovingLeft = false;
+        }
+        else if(collision.gameObject.tag == "EnemyWaypointB"){
+            //print("hit waypoint B");
+            transform.localScale = new Vector3(-1f,1f,1f); // flip the sprite
+            IsMovingRight = false;
+            IsMovingLeft = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //print(collision.gameObject.tag);
+
+        // Essa daqui ainda não está funcionando!!!!!
+
+        if(collision.gameObject.tag == "wall"){
+            //print("hit wall");
+            if(IsMovingRight){
+                transform.localScale = new Vector3(-1f,1f,1f); //   flip the sprite
+                IsMovingRight = false;
+                IsMovingLeft = true;
+            }else if(IsMovingLeft){
+                transform.localScale = new Vector3(1f,1f,1f); // flip the sprite
+                IsMovingRight = true;
+                IsMovingLeft = false;
+            }
+        }
+    }
+
+    private void Walk(){
+        if (IsShooting){
+            return;
+        }
+        else if(IsMovingRight){
+            transform.Translate(Vector2.right * 1f * Time.deltaTime);
+        }else if(IsMovingLeft){
+            transform.Translate(Vector2.left * 1f * Time.deltaTime);
+        }
     }
 
 
     private void OnMouseDown() {
         animator.SetBool("isDead",true);
+        IsDead = true;
+        // disable the collider
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        // disable the rigidbody
+        GetComponent<Rigidbody2D>().simulated = false;
+
     }
 }
