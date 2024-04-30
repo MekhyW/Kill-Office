1c7d9cf8d8010a0b19d15efb274ffae87ca9e856
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2024-04-29 23:01:59 -0300

    explosion animation

--- Assets/Barrel.cs
+++ Assets/Barrel.cs
@@ -6,16 +6,25 @@ public class Barrel : MonoBehaviour
 {
     public GameObject explosionRadius;
     private float radius = 3f;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         explosionRadius.SetActive(false);
+        animator = GetComponent<Animator>();
     }
 
     void OnMouseDown()
     {
         explosionRadius.SetActive(true);
+        StartCoroutine(explode());
+        
+    }
+
+    public IEnumerator explode(){
+        animator.SetBool("explode",true);
+        yield return new WaitForSeconds(0.5f);
         gameObject.SetActive(false);
     }
 }
