792c7a55f70a13062047aa3c595b2ed79b72ee39
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2024-05-01 19:41:48 -0300

    fix barrel prefab and enemy revive behaviour

--- Assets/scripts/Barrel.cs
+++ Assets/scripts/Barrel.cs
@@ -7,14 +7,14 @@ public class Barrel : MonoBehaviour
     public GameObject explosionRadius;
     private float radius = 3f;
     private Animator animator;
-    public AudioClip explodeSfx;
-    public AudioSource audioSource;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         explosionRadius.SetActive(false);
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnMouseDown()
@@ -25,7 +25,7 @@ public class Barrel : MonoBehaviour
     }
 
     public IEnumerator explode(){
-        audioSource.PlayOneShot(explodeSfx,1f);
+        audioSource.PlayOneShot(audioSource.clip,1f);
         animator.SetBool("explode",true);
         yield return new WaitForSeconds(0.5f);
         gameObject.SetActive(false);
