dc03e009849b9cd0dfd5a3ba27339818abb03843
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2024-05-02 09:59:43 -0300

    added end scene

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
+    public string next_level;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class UI_Manager : MonoBehaviour
      public void PlayButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("industrial_level");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(next_level);
     }
 }
 
