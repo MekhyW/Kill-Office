34bb7bfdc67bbc7e4f4cfb640aadab0b99037417
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-05-01 21:31:57 -0300

    Corrigindo a aprenecia e colisão do barril durante o respawn.

--- Assets/scripts/Barrel.cs
+++ Assets/scripts/Barrel.cs
@@ -30,6 +30,10 @@ public class Barrel : MonoBehaviour
         animator.SetBool("explode",true);
         yield return new WaitForSeconds(0.5f);
         //gameObject.SetActive(false);
+        // desabilita o colisor do barril
+        // make the object invisible
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
         invokeRespawn();
     }
 
@@ -40,6 +44,10 @@ public class Barrel : MonoBehaviour
     private void respawn(){
         print("Respawning");
         //gameObject.SetActive(true);
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
         animator.SetBool("explode",false);
+        // retorna o objeto para o estado inicial
+
     }
 }
