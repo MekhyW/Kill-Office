f194529702f0f65257a7efd0131e553daa1ba173
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-06-03 10:14:51 -0300

    Making the stack truly random.

--- Assets/scripts/LevelStack.cs
+++ Assets/scripts/LevelStack.cs
@@ -9,7 +9,9 @@ public class LevelStack : MonoBehaviour
     public int levels_before_boss = 5;
     public Stack<string> levelstack = new Stack<string>();
     private List<string> levels = new List<string> { 
-        "snake_barrels", "snake_enemies", "snake_enemies2", "snake_spikes", "the_pit", "the_pit_barrels", "the_pit_enemies", "the_pit_hard", "the_pit_platform", "the_pit_walls", "easy_level_1", "easy_level_2", "easy_level_3", "easy_level_4", "easy_level_5" 
+        "snake_barrels", "snake_enemies", "snake_enemies2", "snake_spikes", "the_pit", "the_pit_barrels", 
+        "the_pit_enemies", "the_pit_hard", "the_pit_platform", "the_pit_walls", "easy_level_1", 
+        "easy_level_2", "easy_level_3", "easy_level_4", "easy_level_5", "business_level_1", "business_level_2"
     };
     private string bosslevel = "boss_scene";
     private string hub = "hub_scene";
@@ -33,14 +35,27 @@ public class LevelStack : MonoBehaviour
         {
             levelstack.Push(l);
         }
+        // randomize the level order
         for (int i = 0; i < levelstack.Count; i++)
         {
-            int r = Random.Range(0, levelstack.Count-1);
             string temp = levelstack.Pop();
-            levelstack.Push(levelstack.ToArray()[r]);
-            levelstack.Pop();
+            int randomIndex = Random.Range(0, levelstack.Count);
+            Stack<string> tempStack = new Stack<string>();
+            for (int j = 0; j < randomIndex; j++)
+            {
+                tempStack.Push(levelstack.Pop());
+            }
             levelstack.Push(temp);
+            for (int j = 0; j < randomIndex; j++)
+            {
+                levelstack.Push(tempStack.Pop());
+            }
         }
+        // print the levelstack
+        foreach (string l in levelstack)
+            {
+                print(l);
+            }
         print("Levels Prepared");
     }
 
@@ -63,7 +78,8 @@ public class LevelStack : MonoBehaviour
 
     public string LoadHub()
     {
-        SceneManager.LoadScene(hub);
+        levels_before_boss = 5;
+        PrepareLevels();
         return hub;
     }
 }
