6e0ad458a65d5777228d6a8ec0d5b181a6c79025
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-06-03 10:29:35 -0300

    Fixing some levels.

--- Assets/scripts/LevelStack.cs
+++ Assets/scripts/LevelStack.cs
@@ -9,12 +9,13 @@ public class LevelStack : MonoBehaviour
     public int levels_before_boss = 5;
     public Stack<string> levelstack = new Stack<string>();
     private List<string> levels = new List<string> { 
-        "snake_barrels", "snake_enemies", "snake_enemies2", "snake_spikes", "the_pit", "the_pit_barrels", 
+        "snake_barrels", "snake_enemies", "snake_enemies2", "snake_spikes", "the_pit_barrels", 
         "the_pit_enemies", "the_pit_hard", "the_pit_platform", "the_pit_walls", "easy_level_1", 
         "easy_level_2", "easy_level_3", "easy_level_4", "easy_level_5", "business_level_1", "business_level_2"
     };
     private string bosslevel = "boss_scene";
     private string hub = "hub_scene";
+
 
     private void Awake()
     {
@@ -51,11 +52,6 @@ public class LevelStack : MonoBehaviour
                 levelstack.Push(tempStack.Pop());
             }
         }
-        // print the levelstack
-        foreach (string l in levelstack)
-            {
-                print(l);
-            }
         print("Levels Prepared");
     }
 
